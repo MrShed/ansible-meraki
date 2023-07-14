169a37298edcbe5360c6b966b5470bbf25fd884a
MrShed <scottclennell@hotmail.com>
2023-07-14 09:53:59 +0100

    Update meraki_device.py

--- plugins/modules/meraki_device.py
+++ plugins/modules/meraki_device.py
@@ -292,7 +292,7 @@ def main():
 
     query_urls = {'device': '/networks/{net_id}/devices'}
     query_org_urls = {'device': '/organizations/{org_id}/devices'}
-    query_device_urls = {'device': '/networks/{net_id}/devices/{serial}'}
+    query_device_urls = {'device': '/devices/{serial}'}
     query_device_lldp_urls = {'device': '/devices/{serial}/lldpCdp'}
     claim_device_urls = {'device': '/networks/{net_id}/devices/claim'}
     bind_org_urls = {'device': '/organizations/{org_id}/claim'}
